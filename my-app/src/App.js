6f36a94df916fd27096d342425ee5ef3435e255c
Sam <soham.mhatre97@gmail.com>
2024-03-13 15:13:57 -0400

    Created the front end

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Public from "./components/Public";
-import Login from "./components/Login";
+import Login from "./features/auth/Login";
 import DashLayout from "./components/DashLayout";
+import Welcome from "./features/auth/Welcome";
+import NoteList from "./features/notes/NoteList";
+import UserList from "./features/users/UserList";
 
 function App() {
   return (
@@ -10,7 +13,15 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
-        <Route path="dash" element={<DashLayout />}></Route>
+        <Route path="dash" element={<DashLayout />}>
+          <Route index element={<Welcome />} />
+          <Route path="notes">
+            <Route index element={<NoteList />} />
+          </Route>
+          <Route path="users">
+            <Route index element={<UserList />} />
+          </Route>
+        </Route>
       </Route>
     </Routes>
   );

63e853b8f4af9b79f8336fac79a341b95bf27271
Sam <soham.mhatre97@gmail.com>
2024-03-27 20:50:57 -0400

    Permissions in React JS

--- my-app/src/features/notes/NotesList.js
+++ my-app/src/features/notes/NotesList.js
@@ -28,9 +28,18 @@ const NotesList = () => {
   if (isSuccess) {
     const { ids, entities } = notes;
 
-    const tableContent = ids?.length
-      ? ids.map((noteId) => <Note key={noteId} noteId={noteId} />)
-      : null;
+    let filteredIds;
+    if (isManager || isAdmin) {
+      filteredIds = [...ids];
+    } else {
+      filteredIds = ids.filter(
+        (noteId) => entities[noteId].username === username
+      );
+    }
+
+    const tableContent =
+      ids?.length &&
+      filteredIds.map((noteId) => <Note key={noteId} noteId={noteId} />);
 
     content = (
       <table className="table table--notes">
